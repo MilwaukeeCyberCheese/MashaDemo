0de23ca36f59436a26daf801ae25a9a577b332e0
Sam Freund <techguy763@gmail.com>
2025-02-13 16:06:33 -0600

    remove a bunch of shit we're not using (#58)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,13 +21,9 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
 import frc.robot.subsystems.AlgaeHandlerSubsystem.AlgaeHandlerIntakeState;
 import frc.robot.subsystems.AlgaeHandlerSubsystem.AlgaeHandlerPositionState;
 import frc.robot.subsystems.ClimberSubsystem.ClimberState;
@@ -363,53 +359,6 @@ public final class Constants {
     }
   }
 
-  public static final class MAXSwerveModule {
-    public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
-    public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
-
-    static {
-      // Use module constants to calculate conversion factors and feed forward gain.
-      double drivingFactor =
-          ModuleConstants.kWheelDiameterMeters * Math.PI / ModuleConstants.kDrivingMotorReduction;
-      double turningFactor = kTau;
-      double drivingVelocityFeedForward = 1 / ModuleConstants.kDriveWheelFreeSpeedRps;
-
-      drivingConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50);
-      drivingConfig
-          .encoder
-          .positionConversionFactor(drivingFactor) // meters
-          .velocityConversionFactor(drivingFactor / 60.0); // meters per second
-      drivingConfig
-          .closedLoop
-          .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-          // These are example gains you may need to them for your own robot!
-          .pid(0.04, 0, 0)
-          .velocityFF(drivingVelocityFeedForward)
-          .outputRange(-1, 1);
-
-      turningConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(20);
-      turningConfig
-          .absoluteEncoder
-          // Invert the turning encoder, since the output shaft rotates in the opposite
-          // direction of the steering motor in the MAXSwerve Module.
-          .inverted(true)
-          .positionConversionFactor(turningFactor) // radians
-          .velocityConversionFactor(turningFactor / 60.0); // radians per second
-      turningConfig
-          .closedLoop
-          .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-          // These are example gains you may need to them for your own robot!
-          .pid(1, 0, 0)
-          .outputRange(-1, 1)
-          // Enable PID wrap around for the turning motor. This will allow the PID
-          // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
-          // to 10 degrees will go through 0 rather than the other direction which is a
-          // longer route.
-          .positionWrappingEnabled(true)
-          .positionWrappingInputRange(0, turningFactor);
-    }
-  }
-
   public static final class DriveConstants {
 
     public static final BooleanSupplier kRateLimitsEnabled = () -> true;
@@ -418,95 +367,5 @@ public final class Constants {
     // the robot, rather the allowed maximum speeds
     public static final double kMaxSpeedMetersPerSecond = 4.8;
     public static final double kMaxAngularSpeed = kTau; // radians per second
-
-    public static final double kDirectionSlewRate = 1.2; // radians per second
-    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
-    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
-
-    public static final double kSlowModifier = 0.25;
-    public static final double kTurningSpeedModifier = 0.5;
-
-    // TODO: what is it?
-    // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(24.5);
-    // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(28.5);
-    // Distance between front and back wheels on robot
-    // TODO: make it work with choreo
-    public static final SwerveDriveKinematics kDriveKinematics =
-        new SwerveDriveKinematics(
-            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
-            new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
-            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
-            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
-
-    // Angular offsets of the modules relative to the chassis in radians
-    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
-    public static final double kFrontRightChassisAngularOffset = 0;
-    public static final double kBackLeftChassisAngularOffset = Math.PI;
-    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
-
-    // SPARK MAX CAN IDs
-    // TODO: what are they?
-    public static final int kFrontLeftDrivingCanId = 2;
-    public static final int kRearLeftDrivingCanId = 4;
-    public static final int kFrontRightDrivingCanId = 6;
-    public static final int kRearRightDrivingCanId = 8;
-
-    public static final int kFrontLeftTurningCanId = 1;
-    public static final int kRearLeftTurningCanId = 3;
-    public static final int kFrontRightTurningCanId = 5;
-    public static final int kRearRightTurningCanId = 7;
-
-    // TODO: is it?
-    public static final boolean kGyroReversed = false;
-  }
-
-  public static final class ModuleConstants {
-    // The MAXSwerve module can be configured with one of three pinion gears: 12T,
-    // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
-    // more teeth will result in a robot that drives faster).
-    // TODO: change this depending on what we do
-    public static final int kDrivingMotorPinionTeeth = 14;
-
-    // Calculations required for driving motor conversion factors and feed forward
-    public static final double kDrivingMotorFreeSpeedRps = VortexMotorConstants.kFreeSpeedRpm / 60;
-    public static final double kWheelDiameterMeters = 0.0762;
-    public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
-    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
-    // teeth on the bevel pinion
-    public static final double kDrivingMotorReduction =
-        (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
-    public static final double kDriveWheelFreeSpeedRps =
-        (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters) / kDrivingMotorReduction;
-  }
-
-  public static final class AutoConstants {
-    public static final double kMaxSpeedMetersPerSecond = 3;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-
-    public static final double kPXController = 1;
-    public static final double kPYController = 1;
-    public static final double kPThetaController = 1;
-
-    // Constraint for the motion profiled robot angle controller
-    public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
-        new TrapezoidProfile.Constraints(
-            kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
-  }
-
-  public static final class NeoMotorConstants {
-    public static final double kFreeSpeedRpm = 5676;
-  }
-
-  public static final class VortexMotorConstants {
-    public static final double kFreeSpeedRpm = 6784;
-  }
-
-  // Not digits cause java doesn't like that
-  public static final class FiveFiftyMotorConstants {
-    public static final double kFreeSpeedRPM = 11000;
   }
 }
