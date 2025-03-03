faed98a7663de958837b4bddc06f30f81860ead8
Sam Freund <techguy763@gmail.com>
2025-03-03 17:48:40 -0600

    elevator (#72)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -99,7 +99,7 @@ public final class Constants {
         new SparkMax(kRightElevatorCANid, MotorType.kBrushless);
 
     public static final SparkLimitSwitch kElevatorLimitSwitch =
-        kLeftElevatorSparkMax.getReverseLimitSwitch();
+        kRightElevatorSparkMax.getReverseLimitSwitch();
 
     public static final SparkMaxConfig kLeftElevatorConfig = new SparkMaxConfig();
     public static final SparkMaxConfig kRightElevatorConfig = new SparkMaxConfig();
@@ -110,7 +110,7 @@ public final class Constants {
 
     // only one cause we slave the other motor to this one
     public static final SparkClosedLoopController kElevatorController =
-        kLeftElevatorSparkMax.getClosedLoopController();
+        kRightElevatorSparkMax.getClosedLoopController();
 
     // TODO: veloc and accel is in inches per second and inches per second squared
     public static final PIDConstants kElevatorPIDConstants =
@@ -148,11 +148,11 @@ public final class Constants {
       kLeftElevatorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).inverted(kLeftInverted);
       kRightElevatorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).inverted(kRightInverted);
 
-      kRightElevatorConfig.follow(
+      kLeftElevatorConfig.follow(
           kLeftElevatorSparkMax); // you can pass in an inverted value after the
       // kLeftElevatorSparkMax, but idk quite how that works yet
 
-      kLeftElevatorConfig
+      kRightElevatorConfig
           .closedLoop
           .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
           .pid(kElevatorPIDConstants.kP, kElevatorPIDConstants.kI, kElevatorPIDConstants.kD)
@@ -160,10 +160,10 @@ public final class Constants {
           .maxAcceleration(kElevatorPIDConstants.kMaxAcceleration)
           .maxVelocity(kElevatorPIDConstants.kMaxVelocity);
 
-      kLeftElevatorConfig.encoder.positionConversionFactor(kConversionFactor);
+      kRightElevatorConfig.encoder.positionConversionFactor(kConversionFactor);
 
       // TODO: see if this is right
-      kLeftElevatorConfig
+      kRightElevatorConfig
           .limitSwitch
           .reverseLimitSwitchEnabled(true)
           .reverseLimitSwitchType(Type.kNormallyOpen);
